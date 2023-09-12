fc2892ca20e7f738c917483acbd0972ce029d74f
Nicolas Migues <109447945+raitoo97@users.noreply.github.com>
2023-09-11 22:22:00 -0300

    FinishCamera

--- Assets/FollowCamera/FollowPj.cs
+++ Assets/FollowCamera/FollowPj.cs
@@ -2,12 +2,22 @@ using UnityEngine;
 public class FollowPj : MonoBehaviour
 {
     [SerializeField] private Transform Pj;
+    private Camera camera;
     void Start()
     {
         Pj = GameObject.FindObjectOfType<MoveSphere>().GetComponent<Transform>();
+        camera = this.GetComponent<Camera>();
     }
     private void Update()
     {
         this.transform.LookAt(Pj);
+        if (Input.GetKey(KeyCode.P))
+        {
+            camera.fieldOfView += 0.5f;
+        }
+        if (Input.GetKey(KeyCode.H))
+        {
+            camera.fieldOfView -= 0.5f;
+        }
     }
 }
