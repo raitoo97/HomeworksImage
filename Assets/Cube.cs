e1035e36d88a02d9d67dc2fbd223145af512fb96
Nicolas Migues <109447945+raitoo97@users.noreply.github.com>
2023-09-14 00:45:12 -0300

    CompleteMatrix

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class Cube : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private int index;
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         
     }
+    public int INDEX { get { return index; } }
 }
--- Assets/Matrix.cs
+++ Assets/Matrix.cs
@@ -1,13 +1,28 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Matrix : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]private List<Cube> cubes;
+    private readonly int size = 3;
+    private Cube[,] cube_matrix;
     void Start()
     {
-        
+        cube_matrix = new Cube[size, size];
+        cubes = new List<Cube>(GameObject.FindObjectsOfType<Cube>());
+        cubes = cubes.OrderBy(c => c.INDEX).ToList();
+        int list = 0;
+        for(int i = 0; i < size; i++)
+        {
+            for(int x = 0; x < size; x++)
+            {
+                cube_matrix[i,x] = cubes[list];
+                list++;
+                print(cube_matrix[i,x]);
+            }
+        }
+
     }
 
     // Update is called once per frame
